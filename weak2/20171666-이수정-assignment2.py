0a2cb68553c60139d13db9225a298b011ae93d30
Sujeong Lee <ekfqlc0001@kookmin.ac.kr>
2017-09-09 20:39:56 +0900

    added 20171666's modified completed assignment

--- weak2/20171666-이수정-assignment2.py
+++ weak2/20171666-이수정-assignment2.py
@@ -9,18 +9,28 @@ def factorial(a):  # factorial 구현.
 
 while True:  # input 값을 int로 제한시켜줌, stack overflow 참조.
     try:
-        num = int(input("Enter number to start factorial = "))
-        break
+        num = int(input("Enter natural number to start factorial = "))
     except ValueError:
         print("Please enter natural number.")
+        continue
+    if num == -1:
+        print("Factorial Ended.")
+        break
+    elif num >= 0:
+        print(factorial(num))
+    else:
+        print("Please Enter number 0 or over to start factorial.")
 
-while num + 1:  # -1일때 멈추고 아닌 값들에서는 반복동작.
-    print(factorial(num))
-    while True:  # 반복을 줄일 수 없을까?
-        try:
-            num = int(input("Enter number to restart factorial = "))
-            break
-        except ValueError:
-            print("Please enter natural number.")
-else:
-    print("Factorial Ended.")
+"""
+==========2ND WEEK HOMEWORK COMMENT============
+++While TRUE 를 사용하지 않기 위하여 while NUM + 1: 를 사용하여 -1이 되는 경우를
+else로 보내려고 했으나, -1 이하의 정수가 들어가는 경우에서 반복문을 끝내지 
+않고 다시 입력받는 방법을 찾지 못했습니다. 5조의 다른 팀원의 코드를 참조하여
+ 최종코드를 완성했습니다.(while TRUE를 사용함.)
+++try, except 구문으로 int 이외의 값이 입력된 경우를 걸러냈습니다.
+++factorial은 함수로 처리하여 코드 맨 위에 작성하였습니다.
+++다음번 코드리뷰에서는 모든 팀원의 사례를 더 자세히 살펴보고 논의하여 가장
+이상적인 코드를 만들 수 있도록 하겠습니다. 
+++감사합니다.
+===============================================
+"""
