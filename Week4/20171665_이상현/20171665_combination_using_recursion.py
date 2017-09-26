331b722d863f26502dca97b366fdbad9f9a18413
tkdgusis4 <tkdgusis4@kookmin.ac.kr>
2017-09-26 09:32:04 +0900

    Update 20171665_combination_using_recursion.py

--- Week4/20171665_이상현/20171665_combination_using_recursion.py
+++ Week4/20171665_이상현/20171665_combination_using_recursion.py
@@ -22,7 +22,7 @@ while True:                             #무한 반복 프로그램 생성
 
         result = combi(n, m)
 
-        print("%d 개의 원소를 가지는 집합에서 %d 개의 원소를 고르는 경우의 수는 %f 입니다." %(n, m, result))
+        print("%d 개의 원소를 가지는 집합에서 %d 개의 원소를 고르는 경우의 수는 %d 입니다." %(n, m, result))
 
     except ValueError:                      #정수 외의 다른 타입 입력 시 예외 처리
         print("정확한 값을 입력해주세요.")
