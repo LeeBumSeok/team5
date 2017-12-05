68886e9b2146c3b5457067cbe0461687c681293f
asteroid-612 <ekfqlc0001@kookmin.ac.kr>
2017-12-05 09:06:37 +0900

    20171666 이수정

--- Week14/20171665_LeeSangHyeon/testGuess.py
+++ Week14/20171665_LeeSangHyeon/testGuess.py
@@ -2,10 +2,12 @@ import unittest
 
 from guess import Guess
 
+
 class TestGuess(unittest.TestCase):
 
     def setUp(self):
         self.g1 = Guess('default')
+        self.g2 = Guess('self')
 
     def tearDown(self):
         pass
@@ -18,13 +20,12 @@ class TestGuess(unittest.TestCase):
         self.assertEqual(self.g1.displayCurrent(), '_ e _ a _ _ t ')
         self.g1.guess('u')
         self.assertEqual(self.g1.displayCurrent(), '_ e _ a u _ t ')
-        self.g1.guess('z')
+        self.g1.guess('r')  # 속하지 않을때
         self.assertEqual(self.g1.displayCurrent(), '_ e _ a u _ t ')
-        self.g1.guess('b')
-        self.assertEqual(self.g1.displayCurrent(), '_ e _ a u _ t ')
-        self.g1.guess('a')
-        self.assertEqual(self.g1.displayCurrent(), '_ e _ a u _ t ')
-
+        self.g2.guess('t')
+        self.assertEqual(self.g2.displayCurrent(), '_ e _ _ ')
+        self.g2.guess('f')
+        self.assertEqual(self.g2.displayCurrent(), '_ e _ f ')
 
     def testDisplayGuessed(self):
         self.assertEqual(self.g1.displayGuessed(), ' e n ')
@@ -34,46 +35,22 @@ class TestGuess(unittest.TestCase):
         self.assertEqual(self.g1.displayGuessed(), ' a e n t ')
         self.g1.guess('u')
         self.assertEqual(self.g1.displayGuessed(), ' a e n t u ')
-        self.g1.guess('z')
-        self.assertEqual(self.g1.displayGuessed(), ' a e n t u z ')
-        self.g1.guess('b')
-        self.assertEqual(self.g1.displayGuessed(), ' a b e n t u z ')
-        self.g1.guess('a')
-        self.assertEqual(self.g1.displayGuessed(), ' a b e n t u z ')
-
-    def testFinished(self):
-        self.g1.currentStatus = 'default'
-        self.assertTrue(self.g1.finished())
-        self.g1.currentStatus = 'defauld'
-        self.assertFalse(self.g1.finished())
-        self.g1.currentStatus = ''
-        self.assertFalse(self.g1.finished())
+        self.g1.guess('r')  # 상관없이 새로운 문자가 들어가야할 곳에 들어가는지?
+        self.assertEqual(self.g1.displayGuessed(), ' a e n r t u ')
+        self.g2.guess('t')
+        self.assertEqual(self.g2.displayGuessed(), ' e n t ')
+        self.g2.guess('f')
+        self.assertEqual(self.g2.displayGuessed(), ' e f n t ')
 
     def testGuess(self):
-        self.assertIsNotNone(self.g1.guessedChars)
-        self.assertEqual(self.g1.guessedChars, { '', 'e', 'n'})
-        self.assertEqual(self.g1.currentStatus, '_e_____')
+        self.assertEqual(self.g1.guess('a'), True)
+        self.assertEqual(self.g1.guess('g'), False)
+        self.assertEqual(self.g1.guess('x'), False)
+        self.assertEqual(self.g2.guess('t'), False)
+        self.assertEqual(self.g2.guess('f'), True)
 
-        self.assertTrue(self.g1.guess('a'))
-        self.assertEqual(self.g1.guessedChars, {'a', '', 'e', 'n'})
-        self.assertEqual(self.g1.currentStatus, '_e_a___')
-
-        self.assertTrue(self.g1.guess('t'))
-        self.assertEqual(self.g1.guessedChars, {'t', 'a', '', 'e', 'n'})
-        self.assertEqual(self.g1.currentStatus, '_e_a__t')
-
-        self.assertTrue(self.g1.guess('u'))
-        self.assertEqual(self.g1.guessedChars, {'u', 't', 'a', '', 'e', 'n'})
-        self.assertEqual(self.g1.currentStatus, '_e_au_t')
-
-        self.assertFalse(self.g1.guess('b'))
-        self.assertEqual(self.g1.guessedChars, {'b', 'u', 't', 'a', '', 'e', 'n'})
-        self.assertEqual(self.g1.currentStatus, '_e_au_t')
-
-        self.assertFalse(self.g1.guess('z'))
-        self.assertEqual(self.g1.guessedChars, {'z', 'b', 'u', 't', 'a', '', 'e', 'n'})
-        self.assertEqual(self.g1.currentStatus, '_e_au_t')
-
+    # def testWord(self):
+    #     self.assertEqual(self.g2.test(), 'default')
 
 if __name__ == '__main__':
     unittest.main()
